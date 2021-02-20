1f292c95a207be753ebe2587687e9565266f17f8
Erdoğan <erdogglc@gmail.com>
2021-02-20 21:38:25 +0300

    Add project files.

---
+++ MyFinalProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFinalProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
